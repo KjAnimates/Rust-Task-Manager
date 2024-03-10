9bd6383e11eb5882ed460a4017d4df5ed90c89c7
KjAnimates <kjhouser43@gmail.com>
2024-03-09 19:06:33 -0500

    add task list

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 mod task;
 use task::Task;
+use std::io;
+
+/// Creates a new task struct.
+fn new_task(name: &str) -> Task {
+    Task { name: name.to_string() }
+}
 
 fn main() {
-    let t: Task = Task { name: "Task".to_string() };
-    t.display();
+    let mut tasks: Vec<Task> = vec![];
+    let t1 = new_task("test");
+
+    // Ask for name.
+    let mut username = String::new();
+    let _ = io::stdin().read_line(&mut username);
+    
+    tasks.push(t1);
+
+    for task in tasks {
+        println!("{}", &task.name);
+    }
 }
